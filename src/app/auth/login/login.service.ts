a2f29803bd7e2f3ad1f9837238cf695fe634d104
Arya Kusuma <arya@badr-interactive.com>
2017-09-04 11:14:51 +0700

    fix import observable

--- src/app/auth/login/login.service.ts
+++ src/app/auth/login/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Login } from './login';
 
