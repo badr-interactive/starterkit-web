4485c794440b9866ed6bcfa72c258960c1ae6be9
Arya Kusuma <arya@badr-interactive.com>
2017-09-14 10:00:36 +0700

    add logout to header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit  {
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/login';
+  }
 
   ngOnInit() {
   }
 
+  logout () {
+    localStorage.removeItem('access_token');
+    window.location.href = this.returnUrl;
+  }
 }
